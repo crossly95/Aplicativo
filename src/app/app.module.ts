b04b7f044c3972ab214d1dce1953932b5f058c41
Oscar Lobaton <u1201508@unimilitar.edu.co>
2018-06-25 13:18:00 -0500

    Primeros cambios App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { GenerarqrPage } from '../pages/generarqr/generarqr';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,6 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ListPage,
+    GenerarqrPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +33,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ListPage,
+    GenerarqrPage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/generarqr/generarqr.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GenerarqrPage } from './generarqr';
+
+@NgModule({
+  declarations: [
+    GenerarqrPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GenerarqrPage),
+  ],
+})
+export class GenerarqrPageModule {}
